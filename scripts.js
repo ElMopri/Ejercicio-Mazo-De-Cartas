98840b0f30c183c8b076ff336d7ebc77ca9ce5a9
ElMopri <alopeztriana1@gmail.com>
2024-09-14 23:19:44 -0500

    dashboard

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    var loginForm = document.getElementById("loginForm");
+    if(loginForm){
+        loginForm.addEventListener("submit",function(event){
+            event.preventDefault();
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
+
+            //Verificacion de usuario y contraseña
+            if(username == "admin" && password=="1234"){
+                window.location.href = "dashboard.html";
+            } else {
+                alert("Usuario o contraseña incorrectos");
+            }
+        });
+    }
+
+})
